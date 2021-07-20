730438c17bdc74c907e09656b2dc2339b3a05ad3
Prakhar Rai <58136319+prakharrai1609@users.noreply.github.com>
2021-07-20 14:35:55 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -11,6 +11,20 @@ const clocker = document.getElementById('clock-buttons');
             document.getElementById("time").textContent = time;
     }
 
+let check = 1;
 
+dark.addEventListener("click", () => {
+        if (check) {
+                document.body.style.backgroundColor = "#444444";
+                document.body.style.color = "#EDEDED";
+                dark.textContent = "☀";
+                check--;
+        } else {
+                document.body.style.backgroundColor = "#EDEDED";
+                document.body.style.color = "#444444";
+                dark.textContent = "🌑";
+                check++;
+        }
+})
 
 
